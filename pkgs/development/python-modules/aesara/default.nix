7eaae759425e4b0738325a8e2bfbf749d6e38265
Frederik Rietdijk <fridh@fridh.nl>
2022-10-27 10:01:51 +0200

    python3Packages.aesara: 2.8.4 -> 2.8.7

--- pkgs/development/python-modules/aesara/default.nix
+++ pkgs/development/python-modules/aesara/default.nix
@@ -21,7 +21,7 @@
 
 buildPythonPackage rec {
   pname = "aesara";
-  version = "2.8.4";
+  version = "2.8.7";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -30,7 +30,7 @@ buildPythonPackage rec {
     owner = "aesara-devs";
     repo = "aesara";
     rev = "refs/tags/rel-${version}";
-    hash = "sha256-Mt1IweQkPqxv+ynezdFHTJXU/oTOwhPkY49GzFJpPaM=";
+    hash = "sha256-xtnz+qKW2l8ze0EXdL9mkx0MzfAnmauC9042W2cVc5o=";
   };
 
   nativeBuildInputs = [
