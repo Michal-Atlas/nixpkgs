4f3db11038fcfad4b57b9267a77431818ac0b16a
Frederik Rietdijk <fridh@fridh.nl>
2022-10-27 10:02:02 +0200

    python3Packages.thinc: 8.1.1 -> 8.1.5

--- pkgs/development/python-modules/thinc/default.nix
+++ pkgs/development/python-modules/thinc/default.nix
@@ -30,14 +30,14 @@
 
 buildPythonPackage rec {
   pname = "thinc";
-  version = "8.1.1";
+  version = "8.1.5";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-m5AoKYTzy6rJjgNn3xsa+eSDYjG8Bj361yQqnQ3VK80=";
+    sha256 = "sha256-TT5N4z0tDq58FFXGDGgORTsCBMKePS1UjXqef+CMz70=";
   };
 
   postPatch = ''
